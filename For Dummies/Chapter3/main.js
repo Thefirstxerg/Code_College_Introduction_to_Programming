18aba815c177c0bc90fbf5d80a698f9720729afc
Thefirstxerg <thefirstxerg@gmail.com>
2025-01-20 13:26:18 +0200

    Chapter3

---
+++ For Dummies/Chapter3/main.js
@@ -0,0 +1,11 @@
+var name;
+
+name = 'Ulrich';
+
+console.log(name.length);
+console.log(name.indexOf('r'));
+
+var userprompt = prompt('What is your name?');
+alert('Hello ' + userprompt);
+
+document.write('Hello, welcome to the document ' + userprompt);
