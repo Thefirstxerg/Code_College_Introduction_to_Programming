0d1cfb58cf72d18800d871208ddb75bfd3b8e77c
Thefirstxerg <thefirstxerg@gmail.com>
2025-01-20 11:41:03 +0200

    Update main.js

---
+++ For Dummies/Chapter2/main.js
@@ -0,0 +1,11 @@
+// alert ("Hello, World!");
+
+for (var i = 0; i < 300; i++) {
+  document.write("Coding is fun!");
+}
+
+/*
+This is a comment
+*/
+ console.log("This is a 'quote' inside a quote");
+ console.log('This is a "quote" inside a quote');
