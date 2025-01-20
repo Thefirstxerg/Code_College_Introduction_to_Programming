e4d947267e1bd54e36f508e71b67aec6d7c1dbe6
Thefirstxerg <thefirstxerg@gmail.com>
2025-01-20 10:06:17 +0200

    Completed Chapter

--- For Dummies/Chapter1/main.js
+++ For Dummies/Chapter1/main.js
@@ -2,4 +2,7 @@ var string1 = "Hello";
 var string2 = "Ulrich";
 
 var complete = string1 + " " + string2;
+var veryComplicatedMathFormula = (5 + 5) * 10 / 2; 
+
 console.log(complete);
+console.log(veryComplicatedMathFormula);
