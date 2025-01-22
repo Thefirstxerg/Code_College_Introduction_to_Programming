c441635c07c7b415f648fbdc14779dd881dac7bb
Thefirstxerg <thefirstxerg@gmail.com>
2025-01-22 10:37:31 +0200

    Update

--- For Dummies/Chapter8/car.js
+++ For Dummies/Chapter8/car.js
@@ -1,15 +1,37 @@
 var dreamCar = {
-    make: "Oldsmobile",
-    model: "98",
-    color: "brown",
-    year: 1983,
-    bodyStyle: "Luxury Car",
-    price: 4500
+    make: "Your mother",
+    model: "122",
+    color: "blue",
+    year: 2015,
+    bodyStyle: "Very Expensive Car",
+    price: 1000000
    };
 
    alert("The type of dreamCar is: " + typeof dreamCar);
 
-// document.getElementById("pricetag").innerHTML = dreamCar.price;
-// document.getElementById("modelyear").innerHTML = dreamCar.year;
-// document.getElementById("body").style.backgroundColor = dreamCar.color;
-// document.getElementById("body").innerHTML = dreamCar.make + " " + dreamCar.model;
+document.getElementById("pricetag").innerHTML = dreamCar.price;
+document.getElementById("modelyear").innerHTML = dreamCar.year;
+document.getElementById("body").style.backgroundColor = dreamCar.color;
+document.getElementById("body").innerHTML = dreamCar.make + " " + dreamCar.model;
+
+var object = {
+    key1 : "value",
+    key2 : 100,
+    key3 : true
+};
+
+var array = [1, 2, 3, 4, 5];
+
+//Making the car move right with onclick event
+car.addEventListener("click", moveRight);
+var moveRightInterval;
+
+function moveRight() {
+    clearInterval(moveRightInterval);
+    var left = 0;
+    moveRightInterval = setInterval(frame, 20);
+    function frame() {
+        left++;
+        car.style.left = left + "px";
+    }
+};
