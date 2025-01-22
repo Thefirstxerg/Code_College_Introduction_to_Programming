64a5b384408024e60bfe3c9cde9e10d5d1189b6e
Thefirstxerg <thefirstxerg@gmail.com>
2025-01-22 13:41:09 +0200

    Update

--- For Dummies/Chapter11/chap11.js
+++ For Dummies/Chapter11/chap11.js
@@ -1,40 +1,41 @@
+//Arrays
 var people = ["Mary", "Bobby", "Judy", "Eddie", "Herbie", "Paul", "Bobby"];
 var otherPeople = ["Teddy", "Goergie", "Katty", "Jack"];
 
-//Examples of every array methods
+//Examples of every array method
+
 //Concat is used to join two arrays
 people = people.concat(otherPeople);
 //IndexOf is used to find the index of a value in an array
-people = people.indexOf("Eddie");
+var eddieIndex = people.indexOf("Eddie");
 //Join is used to join the elements of an array into a string
-people = people.join(" # ");
+var peopleString = people.join();
 //LastIndexOf is used to find the last index of a value in an array
-people = people.lastIndexOf("Bobby");
+var lastBobbyIndex = people.lastIndexOf("Bobby");
 //Pop is used to remove the last element of an array
-people = people.pop();
+people.pop();
 people.pop();
 //Push is used to add an element to the end of an array
-people = people.push("Teddy");
+people.push("Teddy");
 people.push("Teddy");
 //Reverse is used to reverse the order of an array
-people = people.reverse();
+people.reverse();
 //Shift and Unshift is used to remove or add the first element of an array
 people.shift();
 people.unshift("Teddy");
 //Slice is used to extract a part of an array
-people = people.slice(0,3);
+var slicedPeople = people.slice(0, 3);
 //Sort is used to sort the elements of an array numerically according to ASCII values
-people = people.sort();
+people.sort();
 //Splice is used to add or remove elements from an array, a combination of pop and push
-people.splice(1,0,"Cathy");
+people.splice(1, 0, "Cathy");
 
-//Simple example 
-var parents = ["Parent1","Parent2"];
-var friends = ["Friend1","Friend2"];
+//Simple example
+var parents = ["Parent1", "Parent2"];
+var friends = ["Friend1", "Friend2"];
 var family = parents.concat(friends).toString();
 var familyID = document.getElementById("myFamily");
 
 console.log(family);
 
-familyID.innerHTML = "<h1> This is my family: " + family + "</h1>";
- 
+familyID.innerHTML = "<h3> This is my family: " + family + "</h3>";
